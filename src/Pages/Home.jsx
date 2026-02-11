24d9a9d934bd7d9a5bfccdc09e43070934e4b03a
Priyanshi Patadia <patadiapriyanshi12@gmail.com>
2026-02-11 20:50:31 +0530

    Updated Project

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Banner from '../Components/home/Banner'
+import Hero from '../Components/home/Hero'
 
 const Home = () => {
   return (
     <div>
       <Banner />
+      <Hero />
     </div>
   )
 }
