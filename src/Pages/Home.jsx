8ca08a99228fbf6c1d717ce5aefdc3f7ed1dce9c
Priyanshi Patadia <patadiapriyanshi12@gmail.com>
2026-02-12 12:36:32 +0530

    Updated Project

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,6 +3,9 @@ import Banner from '../Components/home/Banner'
 import Hero from '../Components/home/Hero'
 import Features from '../Components/Features'
 import Testimonial from '../Components/home/Testimonial'
+import CallToAction from '../Components/home/CallToAction'
+import { Footprints } from 'lucide-react'
+import Footer from '../Components/home/Footer'
 
 const Home = () => {
   return (
@@ -11,6 +14,8 @@ const Home = () => {
       <Hero />
       <Features />
       <Testimonial />
+      <CallToAction />
+      <Footer />
     </div>
   )
 }
