43c1eca67ade5522c2bc52606c4d663e1dbd6722
Priyanshi Patadia <patadiapriyanshi12@gmail.com>
2026-02-14 08:43:01 +0530

    Updated Project

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,7 +1,21 @@
-import { Plus, PlusIcon, UploadCloudIcon } from 'lucide-react'
-import React from 'react'
+import { FilePenLineIcon, PlusIcon, UploadCloudIcon } from 'lucide-react'
+import React, { use } from 'react'
+import { dummyResumeData } from '../assets/assets'
 
 const Dashboard = () => {
+
+  const colors = ["#9333ea", "#d97706", "#dc2626", "#0284c7", "#16a34a"]
+  const [allResumes, setAllResumes] = useState([])
+
+  const loadAllResumes = async () => {
+    setAllResumes(dummyResumeData)
+  }
+
+  useEffect(() => {
+    loadAllResumes()
+  }, [])
+
+
   return (
     <div>
         <div className='max-w-7x1 mx-auto px-4 py-8'>
@@ -28,6 +42,36 @@ const Dashboard = () => {
           </button>
 
         </div>
+
+        <hr className='border-slate-300 my-6 sm:w-[360px]'/>
+
+        <div className='grid grid-cols-2 sm:flex flex-wrap gap-4'>
+          {allResumes.map((resume, index) => {
+            const baseColor = colors[index % colors.length];
+            return (
+              <button key={index} className='relative w-full sm:max-w-36 h-48 flex 
+              flex-col items-center justify-center rounded-lg gap-2 border group
+              hover:shadow-lg transition-all duration-300 cursor-pointer' 
+              style={{background: `linear-gradient(135deg, ${baseColor}10, ${baseColor}40)`, borderColor: `${baseColor}80`}}>
+
+                <FilePenLineIcon className='size-7 group-hover:scale-105 transition-all' style={{color: baseColor}}/>
+                <p className='text-sm group-hover:scale-105 transition-all px-2 text-center' style={{color: baseColor}}>
+                  {resume.title}</p>
+                <p className='absolute bottom-1 text-[11px] text-slate-400 
+                group-hover:text-slate-500 transition-all duration-300 px-2 
+                text-center' style={{color: baseColor + '90'}}>
+                  Updated on {new Date(resume.updatedAt).toLocaleDateString()}
+                </p>
+                <div>
+                  
+                </div>
+              </button>
+            )
+          })}
+
+        </div>
+
+
         </div>
     </div>
   )
