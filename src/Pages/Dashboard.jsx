26919fa6b931325dc45d05cf36c82bd8289eda28
Priyanshi Patadia <patadiapriyanshi12@gmail.com>
2026-02-15 01:48:22 +0530

    Updated Project

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -32,6 +32,11 @@ const Dashboard = () => {
     navigate('/app/builder/res123')
   }
 
+  const editTitle = async (event) => {
+    event.preventDefault()
+    setShowEditResumeId('')
+  }
+
   useEffect(() => {
     loadAllResumes()
   }, [])
@@ -69,7 +74,7 @@ const Dashboard = () => {
           {allResumes.map((resume, index) => {
             const baseColor = colors[index % colors.length];
             return (
-              <button key={index} className='relative w-full sm:max-w-36 h-48 flex 
+              <button key={index} onClick={() => navigate(`/app/builder/${resume._id}`)} className='relative w-full sm:max-w-36 h-48 flex 
               flex-col items-center justify-center rounded-lg gap-2 border group
               hover:shadow-lg transition-all duration-300 cursor-pointer'
                 style={{ background: `linear-gradient(135deg, ${baseColor}10, ${baseColor}40)`, borderColor: `${baseColor}80` }}>
@@ -83,9 +88,10 @@ const Dashboard = () => {
                   Updated on {new Date(resume.updatedAt).toLocaleDateString()}
                 </p>
 
-                <div className='absolute top-1 right-1 group-hover:flex items-center hidden'>
+                <div onClick={e => e.stopPropagation()} className='absolute top-1 right-1 group-hover:flex items-center hidden'>
                   <Trash2 className='size-7 p-1.5 hover:bg-white/50 rounded text-slate-700 transition-colors' />
-                  <Pencil className='size-7 p-1.5 hover:bg-white/50 rounded text-slate-700 transition-colors' />
+                  <Pencil onClick={() => { setShowEditResumeId(resume._id); setTitle(resume.title) }}
+                    className='size-7 p-1.5 hover:bg-white/50 rounded text-slate-700 transition-colors' />
                 </div>
               </button>
             )
@@ -136,7 +142,7 @@ const Dashboard = () => {
                     )}
                   </div>
                 </label>
-                <input type="file" id='resume-input' accept='.pdf' hidden
+                <input type="file" id='resume-upload' accept='.pdf' hidden
                   onChange={(e) => setResume(e.target.files[0])} />
               </div>
 
@@ -146,6 +152,25 @@ const Dashboard = () => {
               <XIcon className='absolute top-4 right-4 text-slate-400
               hover:text-slate-600 cursor-pointer transition-colors'
                 onClick={() => { setShowUploadResume(false); setTitle('') }} />
+            </div>
+          </form>
+        )}
+
+        {editResumeId && (
+          <form onSubmit={editTitle} onClick={() => setShowEditResume('')} className='fixed inset-0 
+          bg-black/70 backdrop-blur bg-opacity-50
+            z-10 flex items-center justify-center'>
+            <div onClick={e => e.stopPropagation()} className='relative bg-slate-50 border shadow-md
+            rounded-lg w-full max-w-sm p-6'>
+              <h2 className='text-xl font-bold mb-4'>Edit Resume Title</h2>
+              <input onChange={(e) => setTitle(e.target.value)} value={title} type="text" placeholder='Enter Resume Title' className='w-full 
+              px-4 py-2 mb-4 focus:border-green-600 ring-green-600' required />
+
+              <button className='w-full py-2 bg-green-600 text-white rounded hover:bg-green-700
+              transition-colors'>Edit Resume</button>
+              <XIcon className='absolute top-4 right-4 text-slate-400
+              hover:text-slate-600 cursor-pointer transition-colors'
+                onClick={() => { setShowEditResumeId(''); setTitle('') }} />
             </div>
           </form>
         )}
