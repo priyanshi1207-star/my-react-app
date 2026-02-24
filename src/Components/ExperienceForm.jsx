a18940a40852ad2b5a60e1b3741a19d159c2b760
Priyanshi Patadia <patadiapriyanshi12@gmail.com>
2026-02-25 01:01:21 +0530

    Updated Project

--- src/Components/ExperienceForm.jsx
+++ src/Components/ExperienceForm.jsx
@@ -1,4 +1,4 @@
-import { Briefcase, Plus } from 'lucide-react'
+import { Briefcase, Plus, Sparkles, Trash2 } from 'lucide-react'
 import React from 'react'
 
 const ExperienceForm = ({ data, onChange }) => {
@@ -55,7 +55,68 @@ const ExperienceForm = ({ data, onChange }) => {
                     {data.map((experience, index) => (
                         <div key={index} className='p-4 border
                         border-gray-200 rounded-lg space-y-3'>
+                            <div className='flex justify-between items-start'>
+                                <h4>Experience #{index + 1}</h4>
+                                <button onClick={() => removeExperience(index)}
+                                    className='text-red-500 hover:text-red-700
+                                transition-colors'>
+                                    <Trash2 className='size-4' />
+                                </button>
+                            </div>
 
+                            <div className='grid md:grid-cols-2 gap-3'>
+                                <input
+                                    value={experience.company || ""}
+                                    onChange={(e) => updateExperience(index, "company", e.target.value)}
+                                    type='text'
+                                    placeholder='Company Name'
+                                    className='px-3 py-2 text-sm rounded-lg' />
+
+                                <input
+                                    value={experience.position || ""}
+                                    onChange={(e) => updateExperience(index, "position", e.target.value)}
+                                    type='text'
+                                    placeholder='Job Title'
+                                    className='px-3 py-2 text-sm rounded-lg' />
+
+                                <input
+                                    value={experience.start_date || ""}
+                                    onChange={(e) => updateExperience(index, "start_date", e.target.value)}
+                                    type='month'
+                                    className='px-3 py-2 text-sm rounded-lg' />
+
+                                <input
+                                    value={experience.end_date || ""}
+                                    onChange={(e) => updateExperience(index, "end_date", e.target.value)}
+                                    type='month'
+                                    disabled={experience.is_current}
+                                    className='px-3 py-2 text-sm rounded-lg disabled:bg-gray-100' />
+                            </div>
+
+                            <label>
+                                <input
+                                    type='checkbox'
+                                    checked={experience.is_current || false}
+                                    onChange={(e) => { updateExperience(index, "is_current", e.target.checked ? true : false); }}
+                                    className='rounded border-gray-300 text-blue-600 focus:ring-blue-500' />
+                                <span className='text-sm text-gray-700'>
+                                    Currently Working Here...
+                                </span>
+                            </label>
+
+
+                            <div className='space-y-2'>
+                                <div className='flex items-center justify-between'>
+                                    <label className='text-sm font-medium text-gray-700'>
+                                        Job Description</label>
+                                    <button className='flex items-center gap-1 px-2 py-1
+                                    text-xs bg-purple-100 text-purple-700 rounded hover
+                                    :bg-purple-200 transition-colors disabled:opacity-50'>
+                                        <Sparkles className='w-3 h-3' />
+                                        Enhance with AI
+                                    </button>
+                                </div>
+                            </div>
                         </div>
                     ))}
                 </div>
