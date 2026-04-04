f4577e95b11b2614219165ed2f816641c1e124b2
Priyanshi Patadia <patadiapriyanshi12@gmail.com>
2026-04-04 22:58:49 +0530

    Updated Project

--- Server/Routes/UserRoutes.js
+++ Server/Routes/UserRoutes.js
@@ -2,11 +2,13 @@ import express from 'express';
 // Ensure these match the physical file names exactly
 import { registerUser, loginUser, getUserProfile } from '../Controllers/UserController';
 import protect from '../middlewares/AuthMiddleware';
+import { getUserResumes } from '../Controllers/UserController';
 
 const UserRouter = express.Router();
 
 UserRouter.post('/register', registerUser);
 UserRouter.post('/login', loginUser);
 UserRouter.get('/data', protect, getUserProfile);
+UserRouter.get('/resumes', protect, getUserResumes);
 
 export default UserRouter;
