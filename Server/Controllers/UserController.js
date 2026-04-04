babf94dadcbad10247bba418135028ac72644166
Priyanshi Patadia <patadiapriyanshi12@gmail.com>
2026-04-04 19:44:41 +0530

    Updated Project

--- Server/Controllers/UserController.js
+++ Server/Controllers/UserController.js
@@ -60,4 +60,19 @@ export const loginUser = async (req, res) => {
     } catch (error) {
         res.status(500).json({ message: "Server error", error: error.message });
     }
-}; 
+};
+
+//controller for getting user profile
+//GET /api/users/data
+export const getUserProfile = async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id).select('-password');
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        res.json(user);
+    }
+    catch (error) {
+        res.status(500).json({ message: "Server error", error: error.message });
+    }
+};
---
+++ Server/middlewares/AuthMiddleware.js
@@ -0,0 +1,18 @@
+import jwt from 'jsonwebtoken';
+
+
+const protect = async (req, res, next) => {
+    const token = req.headers.authorization;
+    if (!token) {
+        return res.status(401).json({ message: "Authorization denied" });
+    }
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded.id;
+        next();
+    } catch (error) {
+        res.status(401).json({ message: "Invalid token" });
+    }
+};
+
+export default protect;
