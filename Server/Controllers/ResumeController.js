3f9e0a86fcbc17de70b2e6cc0958f49717a37bad
Priyanshi Patadia <patadiapriyanshi12@gmail.com>
2026-04-05 13:22:27 +0530

    Updated Project

--- Server/Controllers/ResumeController.js
+++ Server/Controllers/ResumeController.js
@@ -4,6 +4,7 @@ import Resume from '../models/Resume.js';
 import User from '../models/User.js';
 
 
+
 export const createResume = async (req, res) => {
     try {
         const { title, summary, skills, experience, projects, education } = req.body;
@@ -56,14 +57,61 @@ export const deleteResume = async (req, res) => {
 };
 
 //Get a specific resume by ID
-//GET: api/resumes/:id
+//GET: api/resumes/get
 export const getResumeById = async (req, res) => {
     try {
         const resume = await Resume.findById(req.params.id);
         if (!resume) {
             return res.status(404).json({ message: "Resume not found" });
         }
-        res.json(resume);
+        resume.__v = undefined;
+        resume.createdAt = undefined;
+        resume.updatedAt = undefined;
+
+        return res.status(200).json({ resume });
+    } catch (error) {
+        res.status(500).json({ message: "Server error", error: error.message });
+    }
+};
+
+//Get resume by ID public
+//GET: api/resumes/public
+export const getResumeByIdPublic = async (req, res) => {
+    try {
+        const resume = await Resume.findById(req.params.id).populate('user', 'name email');
+        if (!resume) {
+            return res.status(404).json({ message: "Resume not found" });
+        }
+        resume.__v = undefined;
+        resume.createdAt = undefined;
+        resume.updatedAt = undefined;
+
+        return res.status(200).json({ resume });
+    } catch (error) {
+        res.status(500).json({ message: "Server error", error: error.message });
+    }
+};
+
+//controller for updating a resume
+//PUT: api/resumes/update
+export const updateResume = async (req, res) => {
+    try {
+        const resume = await Resume.findById(req.params.id);
+        if (!resume) {
+            return res.status(404).json({ message: "Resume not found" });
+        }
+        if (resume.user.toString() !== req.user) {
+            return res.status(401).json({ message: "Unauthorized" });
+        }
+        const { title, summary, skills, experience, projects, education } = req.body;
+        resume.title = title || resume.title;
+        resume.summary = summary || resume.summary;
+        resume.skills = skills || resume.skills;
+        resume.experience = experience || resume.experience;
+        resume.projects = projects || resume.projects;
+        resume.education = education || resume.education;
+        await resume.save();
+        res.json({ message: "Resume updated successfully", resume });
     } catch (error) {
         res.status(500).json({ message: "Server error", error: error.message });
     }
