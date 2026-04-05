4e1209cf22bf7d2a0aec9d9e484ca1f4c7e1dad7
Priyanshi Patadia <patadiapriyanshi12@gmail.com>
2026-04-05 12:31:25 +0530

    Updated Project

--- Server/Controllers/ResumeController.js
+++ Server/Controllers/ResumeController.js
@@ -1,6 +1,8 @@
 //Controller for Creating New Resume
 //POST: api/resumes
 import Resume from '../models/Resume.js';
+import User from '../models/User.js';
+
 
 export const createResume = async (req, res) => {
     try {
@@ -29,6 +31,39 @@ export const getUserResumes = async (req, res) => {
             return res.status(404).json({ message: "User not found" });
         }
         res.json(user.Resume);
+    } catch (error) {
+        res.status(500).json({ message: "Server error", error: error.message });
+    }
+};
+
+
+//controller for deleting a resume
+//DELETE: api/resumes/delete
+export const deleteResume = async (req, res) => {
+    try {
+        const resume = await Resume.findById(req.params.id);
+        if (!resume) {
+            return res.status(404).json({ message: "Resume not found" });
+        }
+        if (resume.user.toString() !== req.user) {
+            return res.status(401).json({ message: "Unauthorized" });
+        }
+        await resume.deleteOne();
+        res.json({ message: "Resume deleted successfully" });
+    } catch (error) {
+        res.status(500).json({ message: "Server error", error: error.message });
+    }
+};
+
+//Get a specific resume by ID
+//GET: api/resumes/:id
+export const getResumeById = async (req, res) => {
+    try {
+        const resume = await Resume.findById(req.params.id);
+        if (!resume) {
+            return res.status(404).json({ message: "Resume not found" });
+        }
+        res.json(resume);
     } catch (error) {
         res.status(500).json({ message: "Server error", error: error.message });
     }
