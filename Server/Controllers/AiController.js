32ccddaa681d14f17d70475890f78e936611c1c6
Priyanshi Patadia <patadiapriyanshi12@gmail.com>
2026-04-06 23:21:16 +0530

    Updated Project

--- Server/Controllers/AiController.js
+++ Server/Controllers/AiController.js
@@ -27,4 +27,33 @@ export const enhanceProfessionalSummary = async (req, res) => {
     } catch (error) {
         res.status(500).json({ message: "Server error", error: error.message });
     }
-}; 
+};
+
+//controller for enhancing a resume's job descriptions using AI
+//POST: api/ai/enhance-job-desc
+export const enhanceJobDescriptions = async (req, res) => {
+    try {
+        const { jobDescriptions } = req.body;
+        if (!jobDescriptions || !Array.isArray(jobDescriptions) || jobDescriptions.length === 0) {
+            return res.status(400).json({ message: "Job descriptions are required and should be an array" });
+        }
+        const response = await openai.chat.completions.create({
+            model: process.env.OPENAI_MODEL,
+            messages: [
+                {
+                    role: "system",
+                    content: 'You are an expert resume writer.Your task is to enhance the job descriptions in a resume. Each job description should be concise, impactful, and highlight the candidate\'s key responsibilities and achievements for that role. Focus on using strong action verbs, quantifying achievements where possible, and tailoring the descriptions to be relevant to a wide range of industries. Avoid generic statements and ensure each job description stands out to potential employers. Make them ATS friendly.',
+                },
+                {
+                    role: "user",
+                    content: JSON.stringify(jobDescriptions),
+                },
+            ],
+        });
+        const enhancedJobDescriptions = JSON.parse(response.choices[0].message.content);
+        return res.status(200).json({ enhancedJobDescriptions });
+    }
+    catch (error) {
+        res.status(500).json({ message: "Server error", error: error.message });
+    }
+};
